f1abf20cdd0a8bd8dcbe86792b872bbd3c474401
Daniel Taylor <Daniel Taylor>
2017-09-09 14:49:55 -0600

    create subject

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,3 +1,4 @@
+import { UsersService } from './../users.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 
@@ -9,7 +10,7 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class UserComponent implements OnInit {
   id: number;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private usersService: UsersService) { }
 
   ngOnInit() {
     this.route.params
@@ -17,7 +18,10 @@ export class UserComponent implements OnInit {
         (params: Params) => {
           this.id = +params['id'];
         }
-      );
+      ); 
+    }
+      onActivate(){ 
+    this.usersService.userActivated.next(this.id); // pass observer that has an argument //pushes data package with this id 
   }
 
 }
